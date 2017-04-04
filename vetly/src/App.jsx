73c68f60a68410b5a854beee7eb2d2ce93266e85
Andreas Terzitane <aterzitane@gmail.com>
2017-04-04 13:31:31 +0200

    checking
    checking

---
+++ vetly/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+    <div className="Container">
+        <div className="Nav">
+        <p>menu</p>
+        </div>
+        <div className="firstslide">
+        </div>
+        <div className="secondslide">
+        </div>
+        <div className="thirdslide">
+        </div>
+        <div className="fourthslide">
+        </div>
+        <div className="footer">
+        </div>
+    </div>    
+     );
+  }
+}
+
+export default App;
