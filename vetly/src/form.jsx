d4781ef2b5a1a707d072a6a73e267d4fb1b4004f
smuckert <magjohansson@outlook.com>
2017-04-27 10:47:17 +0100

    working on the mobile version of form

--- vetly/src/form.jsx
+++ vetly/src/form.jsx
@@ -35,7 +35,14 @@ function cambiarSignUp(at) {
 
 }
 
-
+function clickToggle() {
+    var x = document.getElementById('signup');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
 
 function ocultarLoginSignUp() {
 
@@ -48,11 +55,16 @@ function ocultarLoginSignUp() {
     document.querySelector('.cont_form_login').style.display = "none";
   },500);
 
+
+
+
   }
 
 
 
     return (
+
+      <div>
       <div className="cotn_principal">
 <div className="cont_centrar">
 
@@ -92,7 +104,7 @@ function ocultarLoginSignUp() {
  <div className="cont_form_login">
 <a href="#" onClick={ocultarLoginSignUp}><i className="material-icons">&#xE5C4;</i></a>
    <h2>Logga In</h2>
- <input type="text" placeholder="Email" />
+ <input type="email" placeholder="Email" />
 <input type="password" placeholder="Lösenord" />
 <button className="btn_login" onClick={cambiarLogin}>Logga In</button>
   </div>
@@ -100,10 +112,11 @@ function ocultarLoginSignUp() {
    <div className="cont_form_sign_up">
 <a href="#" onClick={ocultarLoginSignUp}><i className="material-icons">&#xE5C4;</i></a>
      <h2>Skapa Konto</h2>
-<input type="text" placeholder="Email" />
+<input type="email" placeholder="Email" />
 <input type="password" placeholder="Lösenord" />
 <input type="password" placeholder="Bekräfta Lösenord" />
 <button className="btn_sign_up" onClick={cambiarSignUp}>Skapa Konto</button>
+
 
   </div>
 
@@ -111,7 +124,31 @@ function ocultarLoginSignUp() {
 
   </div>
  </div>
+
+
+
 </div>
+
+
+ <div className="form-div">
+ <h3>Kom Igång</h3>
+  <div id="signup"> 
+  <input type="email" placeholder="Email" />
+  <input type="password" placeholder="Lösenord" />
+  <input type="password" placeholder="Bekräfta Lösenord" />
+  <button className="formButton">Skapa Konto</button>
+  </div>
+
+  <div className="login"> 
+  <input type="email" placeholder="Email" />
+  <input type="password" placeholder="Lösenord" />
+  <button className="formButton">Logga In</button>
+  </div>
+</div>
+<button className="formButton" onClick={clickToggle}>toggla här</button>
+</div>
+
+
     );
   }
 }
